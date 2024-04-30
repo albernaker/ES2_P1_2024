f9d9294cde9c0157867624ec69b29ac0ac081d19
albernaker <79996549+albernaker@users.noreply.github.com>
2024-04-30 19:54:20 +0200

    Primer Commit

--- Test/TriangleTest.java
+++ Test/TriangleTest.java
@@ -1,10 +1,17 @@
 import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TriangleTest {
+
+    @BeforeAll
+    static void setUP() {
+    }
+    
+
     private int[] costats = new int[] {2,4,2,3,4};
     @BeforeAll
     static void setUpBeforeAll() {
